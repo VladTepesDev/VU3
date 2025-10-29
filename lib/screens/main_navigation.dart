0ea9141014f139a6d3fb9b998d6e05f23c250151
vlad-ungurean <vlad.ungurean@vebo.io>
2025-10-29 15:31:05 +0200

    redesigned navigation

--- lib/screens/main_navigation.dart
+++ lib/screens/main_navigation.dart
@@ -36,66 +36,62 @@ class MainNavigationState extends State<MainNavigation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBody: true,
+      extendBody: false,
       body: IndexedStack(
         index: _currentIndex,
         children: _screens,
       ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 20),
-        child: SizedBox(
-          height: 70,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(24),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      AppTheme.glassWhite.withValues(alpha: 0.3),
-                      AppTheme.glassGray.withValues(alpha: 0.2),
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(24),
-                  border: Border.all(
-                    color: AppTheme.borderWhite.withValues(alpha: 0.5),
-                    width: 1.5,
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    _buildNavItem(
-                      icon: Icons.home_outlined,
-                      activeIcon: Icons.home,
-                      label: 'Home',
-                      index: 0,
-                    ),
-                    _buildNavItem(
-                      icon: Icons.add_a_photo_outlined,
-                      activeIcon: Icons.add_a_photo,
-                      label: 'Add Meal',
-                      index: 1,
-                    ),
-                    _buildNavItem(
-                      icon: Icons.menu_book_outlined,
-                      activeIcon: Icons.menu_book,
-                      label: 'Plans',
-                      index: 2,
-                    ),
-                    _buildNavItem(
-                      icon: Icons.person_outline,
-                      activeIcon: Icons.person,
-                      label: 'Profile',
-                      index: 3,
-                    ),
-                  ],
-                ),
+      bottomNavigationBar: SizedBox(
+        height: 70,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                AppTheme.glassWhite.withValues(alpha: 0.3),
+                AppTheme.glassGray.withValues(alpha: 0.2),
+              ],
+            ),
+            border: Border(
+              top: BorderSide(
+                color: AppTheme.borderWhite.withValues(alpha: 0.5),
+                width: 1.5,
+              ),
+              bottom: BorderSide(
+                color: AppTheme.borderWhite.withValues(alpha: 0.5),
+                width: 1.5,
               ),
             ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildNavItem(
+                icon: Icons.home_outlined,
+                activeIcon: Icons.home,
+                label: 'Home',
+                index: 0,
+              ),
+              _buildNavItem(
+                icon: Icons.add_a_photo_outlined,
+                activeIcon: Icons.add_a_photo,
+                label: 'Add Meal',
+                index: 1,
+              ),
+              _buildNavItem(
+                icon: Icons.menu_book_outlined,
+                activeIcon: Icons.menu_book,
+                label: 'Plans',
+                index: 2,
+              ),
+              _buildNavItem(
+                icon: Icons.person_outline,
+                activeIcon: Icons.person,
+                label: 'Profile',
+                index: 3,
+              ),
+            ],
           ),
         ),
       ),
